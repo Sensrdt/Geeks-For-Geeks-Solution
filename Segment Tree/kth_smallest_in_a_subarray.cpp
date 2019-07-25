a86dafa0dafa72535ef1e0dbb7fe040c76ad6835
Sridip <stsdutta2@gmail.com>
2019-07-25 14:38:17 +0530

    commit changes

--- Segment Tree/kth_smallest_in_a_subarray.cpp
+++ Segment Tree/kth_smallest_in_a_subarray.cpp
@@ -23,30 +23,30 @@ void build_tree(int node, int a, int b) {
 	build_tree(node*2, a, (a+b)/2); 
 	build_tree(node*2+1, 1+(a+b)/2, b); 
 	
-	tree[node] = max(tree[node*2], tree[node*2+1]); 
+	tree[node] = min(tree[node*2], tree[node*2+1]); 
 }
 
 
-void update_tree(int node, int a, int b, int i, int j, int value) {
+// void update_tree(int node, int a, int b, int i, int j, int value) {
     
-	if(a > b || a > j || b < i) 
-		return;
+// 	if(a > b || a > j || b < i) 
+// 		return;
     
-  	if(a == b) { 
-    		tree[node] += value;
-    		return;
-	}
+//   	if(a == b) { 
+//     		tree[node] += value;
+//     		return;
+// 	}
 
-	update_tree(node*2, a, (a+b)/2, i, j, value); 
-	update_tree(1+node*2, 1+(a+b)/2, b, i, j, value); 
+// 	update_tree(node*2, a, (a+b)/2, i, j, value); 
+// 	update_tree(1+node*2, 1+(a+b)/2, b, i, j, value); 
 
-	tree[node] = max(tree[node*2], tree[node*2+1]); 
-}
+// 	tree[node] = max(tree[node*2], tree[node*2+1]); 
+// }
 
 
 int query_tree(int node, int a, int b, int i, int j) {
 	
-	if(a > b || a > j || b < i) return -inf; 
+	if(a > b || a > j || b < i) return inf; 
 
 	if(a >= i && b <= j) 
 		return tree[node];
@@ -54,7 +54,7 @@ int query_tree(int node, int a, int b, int i, int j) {
 	int q1 = query_tree(node*2, a, (a+b)/2, i, j); 
 	int q2 = query_tree(1+node*2, 1+(a+b)/2, b, i, j); 
 
-	int res = max(q1, q2); 
+	int res = min(q1, q2); 
 	
 	return res;
 }
@@ -64,8 +64,8 @@ int main() {
 	for(int i = 0; i < N; i++) cin>>arr[i];
 
 	build_tree(1, 0, N-1);
-    int ql, qh, val; cin>>ql>>qh>>val;
-	update_tree(1, 0, N-1, ql, qh, val); 
+    // int ql, qh, val; cin>>ql>>qh>>val;
+	// update_tree(1, 0, N-1, ql, qh, val); 
     int qql, qqh; cin>>qql>>qqh;
 	cout << query_tree(1, 0, N-1, qql, qqh) << endl; 
 }
