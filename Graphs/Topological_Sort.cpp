8666946b6ec72c3966e18e0d9a9836fa4ac8ac0e
Sridip <stsdutta2@gmail.com>
2019-08-13 19:35:22 +0530

    Topo Sort

--- Graphs/Topological_Sort.cpp
+++ Graphs/Topological_Sort.cpp
@@ -1,71 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-class graph{
-	int V;
-	list<int> *adj;
-	void toposort(int V, bool visited[], stack<int> &stack);
-public:
-	graph(int V);
 
-	void addEdge(int v, int w);
-
-	void toposort_print();
-};
-
-graph::graph(int V){
-	//cout<<"call 1\n";
-	this -> V = V;
-	adj = new list<int>[V];
-}
-
-void graph::addEdge(int v, int w){
-	//cout<<"call 2\n";
-	adj[v].push_back(w);
-}
-
-void graph::toposort(int V, bool visited[], stack<int> &stack){
-	//cout<<"call 3\n";
-	visited[V] = true;
-
-	list<int>::iterator it;
-	for (it = adj[V].begin(); it != adj[V].end(); it++) if (!visited[*it]) toposort(*it, visited, stack);
-	stack.push(V);
-}
-//here now we need to print the result
-
-void graph::toposort_print(){
-	//cout<<"call 4\n";
-	stack<int> stack;
-
-	//bool visited[V] = false;
-	bool *visited = new bool[V];
-	for (int i = 0; i < V; i++)
-        visited[i] = false;
-	for (int i=0;i<V;i++) {
-		//cout<<"call 4 / 1 \n";
-		if (!visited[i]){
-			//cout<<"call 4 / 2 \n";
-			toposort(i, visited, stack);
+vector<vector<int>> a;
+bool *visited;
+stack<int> s;
+void check(int);
+void toposort(int e)
+{
+	for (int i = 0; i < e; i++)
+	{
+		if (!visited[i])
+		{
+			check(i);
 		}
 	}
-	while (stack.empty() == false) {
-		cout<<stack.top()<<" "<<endl; stack.pop();
+}
+void check(int i)
+{
+	visited[i] = true;
+	for (int j = 0; j < a[i].size(); j++)
+	{
+		int x = a[i][j];
+		if (!visited[x])
+		{
+			toposort(x);
+		}
 	}
+	s.push(i);
 }
 
+void display(int e)
+{
+	for (int i = 0; i < e; i++)
+	{
+		cout << s.top() << " ";
+		s.pop();
+	}
+}
 int main()
 {
-		cout<<"Execution Started\n";
-	  graph g(6);
-    g.addEdge(5, 2);
-    g.addEdge(5, 0);
-    g.addEdge(4, 0);
-    g.addEdge(4, 1);
-    g.addEdge(2, 3);
-    g.addEdge(3, 1);
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int e;
+		cin >> e;
+		int u, v;
+		visited = new bool[n];
+		a.resize(e);
+		for (int i = 0; i < e; i++)
+		{
+			cin >> u >> v;
+			a[u].push_back(v);
+		}
 
-    cout << "Following is a Topological Sort of the given graph \n";
-    g.toposort_print();
-	return 0;
+		toposort(e);
+		display(e);
+	}
 }
